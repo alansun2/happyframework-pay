3d5457c02fd577aa9fc61d985da78dfe0f27b7a9
alan <18698525802@163.com>
2018-07-13 14:48:19 +0800

    add WechatResponseVO @Tostring

--- src/main/java/com/ehu/weixin/entity/WeChatResponseVO.java
+++ src/main/java/com/ehu/weixin/entity/WeChatResponseVO.java
@@ -2,6 +2,7 @@ package com.ehu.weixin.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
@@ -12,23 +13,24 @@ import java.io.Serializable;
  */
 @Setter
 @Getter
-public class WeChatResponseVO implements Serializable{
+@ToString
+public class WeChatResponseVO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private String appId;
-	private String timeStamp;
-	private String nonceStr;
-	private String packageValue;
-	private String partnerId;
-	private String prepayId;
-	private String sign;
-	/**
-	 * 小程序签名类型 MD5
-	 */
-	private String signType;
+    private static final long serialVersionUID = 1L;
+    private String appId;
+    private String timeStamp;
+    private String nonceStr;
+    private String packageValue;
+    private String partnerId;
+    private String prepayId;
+    private String sign;
+    /**
+     * 小程序签名类型 MD5
+     */
+    private String signType;
 
-	@Override
-	public String toString() {
-		return "微信返回返回串：appId:"+appId+",timeStamp:"+timeStamp+",nonceStr:"+nonceStr+",packageValue:"+packageValue+",partnerId:"+partnerId+",prepayId:"+prepayId+",sign:"+sign;
-	}
+    @Override
+    public String toString() {
+        return "微信返回返回串：appId:" + appId + ",timeStamp:" + timeStamp + ",nonceStr:" + nonceStr + ",packageValue:" + packageValue + ",partnerId:" + partnerId + ",prepayId:" + prepayId + ",sign:" + sign;
+    }
 }

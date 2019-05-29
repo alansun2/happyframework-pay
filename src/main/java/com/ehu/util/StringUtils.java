754669a756f364830ed8ee1e707bc0796be371f3
Alan <18698525802@163.com>
2019-05-29 14:43:35 +0800

    update Product

--- src/main/java/com/ehu/util/StringUtils.java
+++ src/main/java/com/ehu/util/StringUtils.java
@@ -107,4 +107,40 @@ public class StringUtils {
     public static String getDefaultIfNull(String str, String defaultVal) {
         return isBlank(str) ? defaultVal : str;
     }
+
+    /**
+     * 按指定的字节数截取字符串（一个中文字符占3个字节，一个英文字符或数字占1个字节）
+     *
+     * @param sourceString 源字符串
+     * @param cutBytes     要截取的字节数
+     * @return 返回截取后String
+     */
+    public static String subStringSpecifyBytes(String sourceString, int cutBytes) {
+        if (StringUtils.isBlank(sourceString)) {
+            return "";
+        }
+
+        int totalBytes = 0, strTotalBytes = sourceString.getBytes().length;
+        if (strTotalBytes <= cutBytes) {
+            return sourceString;
+        }
+
+        int lastIndex = 0, strLength = sourceString.length();
+        int last = strLength - 1;
+        for (int i = last; i >= 0; i--) {
+            String s = Integer.toBinaryString(sourceString.charAt(i));
+            if (s.length() > 8) {
+                totalBytes += 3;
+            } else {
+                totalBytes += 1;
+            }
+
+            if (strTotalBytes - totalBytes <= cutBytes) {
+                lastIndex = i;
+                break;
+            }
+        }
+
+        return sourceString.substring(0, lastIndex);
+    }
 }

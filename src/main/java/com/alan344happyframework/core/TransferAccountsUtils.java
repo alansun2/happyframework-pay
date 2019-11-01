d9cfbdb8fd4545f5815d570b10d220b40814d93d
Alan <18698525802@163.com>
2019-11-01 14:26:37 +0800

    add

---
+++ src/main/java/com/alan344happyframework/core/TransferAccountsUtils.java
@@ -0,0 +1,51 @@
+package com.alan344happyframework.core;
+
+import com.alan344happyframework.bean.PayBase;
+import com.alan344happyframework.bean.PayResponse;
+import com.alan344happyframework.bean.QueryTransferMoneyInternal;
+import com.alan344happyframework.bean.TransferMoneyInternal;
+import com.alan344happyframework.exception.PayException;
+import com.alan344happyframework.weixin.entity.TransferToBankCardParams;
+
+/**
+ * @author AlanSun
+ * @date 2019/7/22 16:50
+ **/
+public class TransferAccountsUtils {
+
+    /**
+     * 第三方内部转账，如果是支付宝就转账至余额，如果是微信就转账至零钱
+     *
+     * @return 如果是微信：SUCCESS：转账成功，PROCESSING：处理中，需要后续处理；其他：失败，需要后续处理
+     */
+    public static PayResponse transferMoneyInternal(TransferMoneyInternal params) throws PayException {
+        return PayIntegrate.getPay(params.getPayType()).transferMoneyInternal(params);
+    }
+
+    /**
+     * 查询内部转账的结果
+     *
+     * @return 如果是微信：SUCCESS：转账成功，PROCESSING：处理中，需要后续处理；其他：失败，需要后续处理
+     */
+    public static PayResponse getResultOfTransferMoneyInternal(QueryTransferMoneyInternal params) throws PayException {
+        return PayIntegrate.getPay(params.getPayType()).getResultOfTransferMoneyInternal(params);
+    }
+
+    /**
+     * 转账到银行卡
+     * <p>
+     * 支付宝未开放该接口
+     */
+    public static PayResponse transferToBankCard(TransferToBankCardParams params) throws PayException {
+        return PayIntegrate.getPay(PayBase.PAY_TYPE_2).transferToBankCard(params);
+    }
+
+    /**
+     * 查询转账到银行卡的结果
+     * <p>
+     * 支付宝未开放该接口
+     */
+    public static PayResponse getResultOfTransferToBank(String orderId) throws PayException {
+        return PayIntegrate.getPay(PayBase.PAY_TYPE_2).getResultOfTransferToBank(orderId);
+    }
+}
---
+++ src/main/java/com/alan344happyframework/weixin/entity/Mch.java
@@ -0,0 +1,15 @@
+package com.alan344happyframework.weixin.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author AlanSun
+ * @date 2019/6/27 16:47
+ **/
+@Getter
+@Setter
+class Mch {
+    private transient int mchNo = 1;
+    private transient int mchAppIdNo = 1;
+}

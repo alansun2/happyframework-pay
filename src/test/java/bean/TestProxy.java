9b4630ff282e78a7e3154c06a09a5aa486313ec0
Alan <18698525802@163.com>
2019-11-06 16:06:52 +0800

    add test

---
+++ src/test/java/bean/TestProxy.java
@@ -0,0 +1,11 @@
+package bean;
+
+/**
+ * @author AlanSun
+ * @date 2019/11/6 11:06
+ */
+public class TestProxy {
+    public void getA(String arg) {
+        System.out.println("sdfsdf" + "," + arg);
+    }
+}

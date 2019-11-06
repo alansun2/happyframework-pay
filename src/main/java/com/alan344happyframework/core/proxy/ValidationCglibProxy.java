74ecea0804d67536791a2bc513a86fce2f2b09d0
Alan <18698525802@163.com>
2019-11-06 16:07:20 +0800

    add 动态代理

---
+++ src/main/java/com/alan344happyframework/core/proxy/ValidationCglibProxy.java
@@ -0,0 +1,32 @@
+package com.alan344happyframework.core.proxy;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+//Cglib动态代理，实现MethodInterceptor接口
+public class ValidationCglibProxy implements MethodInterceptor {
+    private Object target;//需要代理的目标对象
+
+    //重写拦截方法
+    @Override
+    public Object intercept(Object obj, Method method, Object[] arr, MethodProxy proxy) throws Throwable {
+        System.out.println("Cglib动态代理，监听开始！");
+        Object invoke = method.invoke(target, arr);//方法执行，参数：target 目标对象 arr参数数组
+        System.out.println("Cglib动态代理，监听结束！");
+        return invoke;
+    }
+
+    //定义获取代理对象方法
+    public Object getCglibProxy(Object objectTarget) {
+        //为目标对象target赋值
+        this.target = objectTarget;
+        Enhancer enhancer = new Enhancer();
+        //设置父类,因为Cglib是针对指定的类生成一个子类，所以需要指定父类
+        enhancer.setSuperclass(objectTarget.getClass());
+        enhancer.setCallback(this);// 设置回调 
+        return enhancer.create();
+    }
+}
---
+++ src/main/java/com/alan344happyframework/core/proxy/ValidationInvocationHandler.java
@@ -0,0 +1,28 @@
+package com.alan344happyframework.core.proxy;
+
+
+import com.alan344happyframework.util.ValidationUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author AlanSun
+ * @date 2019/11/6 11:15
+ */
+public class ValidationInvocationHandler implements InvocationHandler {
+    private Object target;
+
+    public ValidationInvocationHandler(Object obj) {
+        this.target = obj;
+    }
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+        ValidationUtil.ValidResult validResult = ValidationUtil.validateBean(args[0]);
+        if (validResult.hasErrors()) {
+            throw new IllegalArgumentException(validResult.getErrors());
+        }
+        return method.invoke(target, args);
+    }
+}
